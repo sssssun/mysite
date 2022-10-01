9649bdcb5cfe7cc719c4dbb80fa9c93b40bedb62
sun <sun@example.com>
2022-10-01 16:48:24 +0900

    first commit

---
+++ mysite/urls.py
@@ -0,0 +1,25 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('polls/', include('polls.urls')), 
+    path('admin/', admin.site.urls),
+]
+
+"""최상위 urlConf 에서 url 에 따라 parsing 해서 받아서 path 별로 분기를 시켜줌
+"""
